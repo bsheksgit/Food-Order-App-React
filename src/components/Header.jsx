7aa7d9987a5109bb48c8b7739b1d708af8ac8aa6
Abhishek A B <bsheks@gmail.com>
2025-11-14 01:05:20 +0530

    Added a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.jpg";
+
+export default function Header()
+{
+    return (
+        <>
+        <header id="main-header">
+            <div id="title">
+                <img src= {logo} alt="Food App Logo"/>
+                <h1>Abhishek Food Order</h1>
+            </div>
+        </header>
+        </>
+    );
+}
