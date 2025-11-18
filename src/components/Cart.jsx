cac3457ed700b0660c8fdd3ba52a6b5918b538a2
Abhishek A B <bsheks@gmail.com>
2025-11-19 01:12:29 +0530

    Corrected the cart modal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,35 @@
 import { CartContext } from "../store/CartContext";
 import { useContext, useEffect } from "react";
 import Modal from './Modal'
+import CartItem from "./CartItem";
+import { currencyFormatter } from '../utils/formatting.js';
 
 export default function Cart(){
-    const {cartStatus, setCartStatus} = useContext(CartContext);
-    useEffect(() => {
-        console.log("Cart status:"+cartStatus);
-    }, [cartStatus]);
+
+    const {cartStatus, 
+        setCartStatus, 
+        cartItems,
+        addItemToCart,
+        removeItemFromCart,
+        goToCheckout} = useContext(CartContext);
+
+    const cartTotal = cartItems.reduce((total, item) => total + (item.quantity * item.price), 0);
 
     return(<Modal className="cart" open={cartStatus} onClose={() => setCartStatus(false)}>
         <h2>Your Cart</h2>
         <ul>
-            <li>Hello</li>
+            {cartItems.map((item) => <CartItem key={item.id} name={item.name} quantity={item.quantity} price={item.price} 
+            onDecrease={() => removeItemFromCart(item)} onIncrease={() => addItemToCart(item)}/>)}
         </ul>
-        <button onClick={() => setCartStatus(false)} className="text-button modal-actions">
-            close
-        </button>
+        <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+        <p className="modal-actions">
+            <button onClick={() => setCartStatus(false)} className="text-button">
+            Close
+            </button>
+            {cartItems.length > 0 && (
+            <button className="button" onClick={goToCheckout}>Go to Checkout</button>
+            )}
+        </p>
         </Modal>);
     
 }
